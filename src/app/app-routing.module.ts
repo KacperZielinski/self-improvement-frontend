9c903b36f8e8cd9f3696b88b15a1cfb3231b7c35
Kacper Zielinski <kacper.zielinski@mailfence.com>
2021-02-07 20:15:38 +0100

    Simple routing works

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {SkillComponent} from './core/skill/skill.component';
+import {ProjectComponent} from './core/project/project.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'skills', component: SkillComponent},
+  { path: 'projects', component: ProjectComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
